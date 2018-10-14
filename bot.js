222e9a005dd45610fa3d4987c4cc3bff85e62a92
Daqiux <43902789+Daqiux@users.noreply.github.com>
2018-10-14 10:29:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.enw.BOT_TOKEN);
 
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
